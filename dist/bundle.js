34b0254c2b5e5ce4bb0bfd2bafcf60cd40af308a
Susanna <royalpolaris@gmail.com>
2018-11-29 23:49:15 -0500

    Change placement of call to closePath

--- dist/bundle.js
+++ dist/bundle.js
@@ -211,13 +211,12 @@ class Paddle {
 
   draw() {
     this.ctx.beginPath();
-    // this.ctx.arc(this.canvas.width / 2, this.canvas.height - 15, 30, Math.PI, 2 * Math.PI);
     this.ctx.arc(this.canvas.width / 2, this.canvas.height, 50, Math.PI, 2 * Math.PI);
+    this.ctx.closePath();
     this.ctx.fillStyle = "pink";
     this.ctx.fill();
     this.ctx.strokeStyle = "blue";
     this.ctx.stroke();
-    this.ctx.closePath();
   }
 }
 

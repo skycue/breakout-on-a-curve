61e82c6642c1f65ec6fe53b2befbf6dba53edf80
Susanna <royalpolaris@gmail.com>
2018-11-30 10:19:30 -0500

    Add horizontal position as parameter to constructor function

--- src/paddle.js
+++ src/paddle.js
@@ -1,12 +1,13 @@
 class Paddle {
-  constructor(canvas, ctx) {
+  constructor(canvas, ctx, xPos) {
     this.ctx = ctx;
     this.canvas = canvas;
+    this.x = xPos;
   }
 
   draw() {
     this.ctx.beginPath();
-    this.ctx.arc(this.canvas.width / 2, this.canvas.height, 50, Math.PI, 2 * Math.PI);
+    this.ctx.arc(this.x, this.canvas.height, 50, Math.PI, 2 * Math.PI);
     this.ctx.closePath();
     this.ctx.fillStyle = "pink";
     this.ctx.fill();

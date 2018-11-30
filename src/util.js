146216d26bd5de720fcba238ce6c0c5f177b6771
Susanna <royalpolaris@gmail.com>
2018-11-30 15:16:04 -0500

    Add Util object with function to find distance between points

---
+++ src/util.js
@@ -0,0 +1,11 @@
+const Util = {
+  distance: function(pos1, pos2) {
+    const [x1, y1] = pos1;
+    const [x2, y2] = pos2;
+
+    const dx = Math.abs(x1 - x2);
+    const dy = Math.abs(y1 - y2);
+
+    return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+  }
+}
